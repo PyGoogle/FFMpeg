286fe4831b832ea8a5a342253cafff0ea9781fdc
bogotobogo <k@bogotobogo.com>
2016-03-17 18:51:33 -0700

    dl output size

--- iframe_extract.py
+++ iframe_extract.py
@@ -1,20 +1,16 @@
 '''
 iframe_extract.py - download video and ffmpeg i-frame extraction
+
 Usage: 
 (ex) python iframe_extract.py -u https://www.youtube.com/watch?v=dP15zlyra3c
 
-Test Cases
-python iframe_extract.py -u https://www.youtube.com/watch?v=dP15zlyra3c
-python iframe_extract.py -u https://www.youtube.com/watch?v=usYC_Z36rHw
-python iframe_extract.py -u https://www.youtube.com/watch?v=I4L2XirSJw0
-python iframe_extract.py -u https://www.youtube.com/watch?v=2wCgpdeQWZA
-python iframe_extract.py -u https://www.youtube.com/watch?v=iyisAjHdhas
-python iframe_extract.py -u https://www.youtube.com/watch?v=GqV7KMjw9jY
-python iframe_extract.py -u https://www.youtube.com/watch?v=Yb2okDst0oc
-
 This code does two things:
 1. Download using youtube-dl
+cmd = ['youtube-dl', '-f', videoSize, '-k', '-o', video_out, download_url]
+
 2. Extract i-frames via ffmpeg
+cmd = [ffmpeg,'-i', inFile,'-f', 'image2','-vf',
+        "select='eq(pict_type,PICT_TYPE_I)'",'-vsync','vfr', imgFilenames]
 '''
 
 from __future__ import unicode_literals
@@ -26,11 +22,17 @@ import subprocess
 import argparse
 import glob
 
+if sys.platform == "Windows":
+    FFMPEG_BIN = "ffmpeg.exe"
+    MOVE = "move"
+    MKDIR = "mkdir"
+else:
+    FFMPEG_BIN = "ffmpeg"
+    MOVE = "mv"
+    MKDIR = "md"
 
 
 def iframe_extract(inFile):
-
-# extract i-frame using ffmpeg
 # ffmpeg -i inFile -f image2 -vf \
 #   "select='eq(pict_type,PICT_TYPE_I)'" -vsync vfr oString%03d.png
 
@@ -75,24 +77,36 @@ def get_info_and_download(download_url):
 
     # renaming the file 
     # remove special characters from the file name
+    print('meta[title]=%s' %meta['title'])
     out = ''.join(c for c in meta['title'] if c.isalnum() or c =='-' or c =='_' ) 
+    print('out=%s' %out)
     extension = meta['ext']
     video_out = out + '.' + extension
-    cmd = ['youtube-dl', '-k', '-o', video_out, download_url]
+    print('video_out=%s' %video_out)
+    videoSize = 'bestvideo[height<=540]+bestaudio/best[height<=540]'
+    cmd = ['youtube-dl', '-f', videoSize, '-k', '-o', video_out, download_url]
+    print('cmd=%s' %cmd)
 
     # download the video
     subprocess.call(cmd)
 
     # Sometimes output file has format code in name such as 'out.f248.webm'
     # so, in this case, we want to rename it 'out.webm' 
-    glob_str = '*.' + extension
-    for f in glob.glob(glob_str):
-       if out in f:
-          if os.path.isfile(f):
-             video_out = f
-             break
+    found = False
+    extension_list = ['mkv', 'mp4', 'webm']
+    for e in extension_list:
+       glob_str = '*.' + e
+       for f in glob.glob(glob_str):
+          if out in f:
+             if os.path.isfile(f):
+                video_out = f
+                found = True
+                break
+       if found:
+          break
        
     # call iframe-extraction : ffmpeg
+    print('before iframe_extract() video_out=%s' %video_out)
     iframe_extract(video_out)
     return meta
 
@@ -117,11 +131,35 @@ def check_arg(args=None):
             results.infile,
             results.outfile)
 
-'''
-Usage sample:
-    syntax: python iframe_extract.py -u url
-    (ex) python iframe_extract.py -u https://www.youtube.com/watch?v=dP15zlyra3c
-'''
+
+# Usage sample:
+#    syntax: python iframe_extract.py -u url
+#    (ex) python iframe_extract.py -u https://www.youtube.com/watch?v=dP15zlyra3c
+
 if __name__ == '__main__':
     u,i,o = check_arg(sys.argv[1:])
     meta = get_info_and_download(u)
+
+
+
+
+'''
+-- Test Cases --
+# Fox BBC
+python iframe_extract.py -u https://www.youtube.com/watch?v=dP15zlyra3c
+# If the Moon were replaced with some of our planets
+python iframe_extract.py -u https://www.youtube.com/watch?v=usYC_Z36rHw
+# CERN News - New boson spotted at the LHC: is it the Higgs?
+python iframe_extract.py -u https://www.youtube.com/watch?v=I4L2XirSJw0
+# The Large Hadron Collider Returns in the Hunt for New Physics
+python iframe_extract.py -u https://www.youtube.com/watch?v=2wCgpdeQWZA
+# Cosmic Distance Ladder: Cepheid Variables
+python iframe_extract.py -u https://www.youtube.com/watch?v=iyisAjHdhas
+# Orion - a brief tour
+python iframe_extract.py -u https://www.youtube.com/watch?v=GqV7KMjw9jY
+# Amazing Golden Gate Bridge- San Francisco, CA (From The Above)
+python iframe_extract.py -u https://www.youtube.com/watch?v=Yb2okDst0oc
+# Grand Teton
+python iframe_extract.py -u https://www.youtube.com/watch?v=NCTd1IHChug 
+'''
+
